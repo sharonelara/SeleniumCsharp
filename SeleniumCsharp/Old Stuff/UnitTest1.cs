d201ff66fca145fae4e51fc5f7cd1d816a7d3a8c
Sharon Grossman <shgrossman3@gmail.com>
2022-02-10 11:07:00 +0200

    AddToCart exercise

---
+++ SeleniumCsharp/Old Stuff/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumCsharp
+{
+    [TestFixture]
+    class UnitTest1
+    {
+        [Test]
+        public void TestOpenBrowser()
+        {
+            IWebDriver driver = new ChromeDriver();
+        }
+    }
+}
